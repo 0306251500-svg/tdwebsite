634c5c8d32434eb73106ada34852718c5863a5e0
0306251500-svg <0306251500@caothang.edu.vn>
2026-06-01 21:26:59 +0700

    Update dulieu.js

--- dulieu.js
+++ dulieu.js
@@ -4,7 +4,7 @@ const DANH_SACH_CHUYEN_BAY = [
         title: "👑 SIÊU COMBO WORLD CUP 2026: Trọn Gói Từ A Đến Z (Vé Máy Bay Thương Gia + Visa + Khách Sạn 5⭐ + Vé VIP Xem Tất Cả Các Trận)",
         date: "📅 Thời gian trọn vẹn giải đấu: 11/06/2026 - 19/07/2026",
         price: "200.000.000 đ",
-        image: "img/wc.jpg",
+        image: "wc.jpg",
         badge: "🔥 SIÊU VIP HOT"
     },
     {
@@ -12,7 +12,7 @@ const DANH_SACH_CHUYEN_BAY = [
         title: "🌴 Combo Nghỉ Dưỡng Miami: Khách Sạn View Biển + Vé Xem Trận Tứ Kết",
         date: "📅 Thời gian: 05/07/2026 - 12/07/2026",
         price: "85.000.000 đ",
-        image: "img/wc.jpg",
+        image: "wc.jpg",
         badge: "⭐ WC LUXURY"
     },
     {
@@ -20,7 +20,7 @@ const DANH_SACH_CHUYEN_BAY = [
         title: "🍁 Tour Trải Nghiệm Toronto - Canada: Khách Sạn Trung Tâm + Vé Vòng Bảng",
         date: "📅 Thời gian: 15/06/2026 - 22/06/2026",
         price: "52.000.000 đ",
-        image: "img/wc.jpg",
+        image: "wc.jpg",
         badge: "📉 WC GIÁ TỐT"
     },
 
@@ -29,7 +29,7 @@ const DANH_SACH_CHUYEN_BAY = [
         title: "Chuyến bay TP.HCM ✈️ Hà Nội",
         date: "📅 Khởi hành: Hàng ngày",
         price: "1.250.000 đ",
-        image: "img/hanoi.jpg",
+        image: "hanoi.jpg",
         badge: "Nội Địa"
     },
     {
@@ -37,7 +37,7 @@ const DANH_SACH_CHUYEN_BAY = [
         title: "Chuyến bay Đà Nẵng ✈️ Phú Quốc",
         date: "📅 Khởi hành: Thứ 5 & Chủ Nhật",
         price: "1.850.000 đ",
-        image: "img/dnpq.jpg",
+        image: "dnpq.jpg",
         badge: "Eco Class"
     },
     {
@@ -45,7 +45,7 @@ const DANH_SACH_CHUYEN_BAY = [
         title: "Chuyến bay Hà Nội ✈️ Tokyo (Nhật Bản)",
         date: "📅 Khởi hành: Thứ 3, 6, 7",
         price: "6.500.000 đ",
-        image: "img/hntko.jpg",
+        image: "hntko.jpg",
         badge: "Business"
     }
 ];
